08ff38850dd6f03dd8857ddf1f003402a9e0b20a
Martin Karadzinov <martin@pingdevs.com>
2022-01-24 20:58:17 +0100

    migration image nullable

--- database/migrations/2022_01_24_191228_add_image_to_users_table.php
+++ database/migrations/2022_01_24_191228_add_image_to_users_table.php
@@ -14,7 +14,7 @@ class AddImageToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('image')->after('password');
+            $table->string('image')->after('password')->nullable();
         });
     }
 
